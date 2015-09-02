2770b3cdefe0a2c0e6a3eab15bbf418596a35334
Andrea Michelotti <andrea.michelotti@lnf.infn.it>
2015-09-02 18:17:34 +0200

    BPMSync

---
+++ models/Libera/ChaosAttribute.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   ChaosAttribute.cpp
+ * Author: michelo
+ * 
+ * Created on September 2, 2015, 5:29 PM
+ */
+
+#include "ChaosAttribute.h"
+
+ChaosAttribute::ChaosAttribute() {
+}
+ChaosAttribute::ChaosAttribute(std::string s) {
+}
+ChaosAttribute::ChaosAttribute(const ChaosAttribute& orig) {
+}
+
+ChaosAttribute::~ChaosAttribute() {
+}
+
