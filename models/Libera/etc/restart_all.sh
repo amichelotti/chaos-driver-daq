3b8180a7f1b7fedd79a836f094ab992dc8e68c81
amichelo <andrea.michelotti@lnf.infn.it>
2017-06-07 01:04:36 +0200

    updated list

--- models/Libera/etc/restart_all.sh
+++ models/Libera/etc/restart_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-lista="libera10 libera02 libera03 libera05 libera06 libera07 libera08 libera09 libera01"
+lista="libera13 libera12 libera02 libera03 libera05 libera06 libera07 libera08 libera09 libera01"
 for i in $lista;do
 echo "* connecting to $i"
 ssh root@$i /etc/init.d/chaos-us.sh restart
