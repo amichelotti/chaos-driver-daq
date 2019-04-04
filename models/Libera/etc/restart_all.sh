ac2c1947c2c665a2e3d21c72002674d2a40d5c15
amichelo <andrea.michelotti@lnf.infn.it>
2019-04-04 10:04:20 +0200

    restart redo all the actions

--- models/Libera/etc/restart_all.sh
+++ models/Libera/etc/restart_all.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-lista="libera13 libera12 libera10 libera02 libera03 libera05 libera06 libera07 libera08 libera09 libera01"
+lista="libera05 libera06 libera10 libera02 libera08 libera13 libera12  libera03 libera07  libera09 libera01"
+for i in $lista;do
+    echo "* remount rw"
+    ssh root@$i "mount -o remount ,rw /"
+    echo "* remount rw"
+    ssh root@$i "mount /mnt/chaos"
+    echo "copy ntp servers config"
+    scp -r openntpd/ root@$i:/etc
+    echo "sync time"
+    ssh root@$i "ntpd -s"
+    ssh root@$i /etc/init.d/chaos-us.sh stop
+    sleep 1
+     echo "* starting  chaos $i"
+     ssh root@$i /etc/init.d/chaos-us.sh start
+done
 
 for i in $lista;do
 echo "* connecting to $i"
