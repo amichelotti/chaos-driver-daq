e0021534e109b3f2648cb4f621d10c8ffea79b80
amichelo <andrea.michelotti@lnf.infn.it>
2019-04-07 16:22:42 +0200

    DD to restore trigger mode

--- models/Libera/etc/restart_all.sh
+++ models/Libera/etc/restart_all.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
 lista="libera05 libera06 libera10 libera04 libera02 libera08 libera13 libera12  libera03 libera07  libera09 libera01"
 for i in $lista;do
+    echo "* stopping $i"
+    ssh root@$i /etc/init.d/chaos-us.sh stop    
     echo "* remount rw $i"
     ssh root@$i "mount -o remount ,rw /"
-    echo "* remount rw"
+    echo "* remount chaous"
     ssh root@$i "mount /mnt/chaos"
     echo "copy ntp servers config"
     scp -r openntpd/ root@$i:/etc
     echo "sync time $i"
     ssh root@$i "ntpd -s"
-    ssh root@$i /etc/init.d/chaos-us.sh stop
     sleep 1
-     echo "* starting  chaos $i"
-     ssh root@$i /etc/init.d/chaos-us.sh start
+    echo "* starting  chaos $i"
+    ssh root@$i /etc/init.d/chaos-us.sh start
 done
 
-for i in $lista;do
-echo "* connecting to $i"
-ssh root@$i /etc/init.d/chaos-us.sh stop
-done
- sleep 6
- for i in $lista;do
- echo "* connecting to $i"
- ssh root@$i /etc/init.d/chaos-us.sh start
- done
